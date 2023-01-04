26a1fd0935bf5c62c963dc248f61416d530e76f0
Tim Quigg <timjquigg@gmail.com>
2023-01-03 22:53:38 -0700

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,36 @@
-import '../styles/globals.css'
+import * as React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "../src/theme";
+import createEmotionCache from "../src/createEmotionCache";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  // console.log(props);
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <title>To Do List</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
---
+++ src/theme.js
@@ -0,0 +1,36 @@
+import { Roboto } from "@next/font/google";
+import { createTheme } from "@mui/material/styles";
+
+export const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
+});
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#4056A1",
+    },
+    secondary: {
+      main: "#D79922",
+    },
+    error: {
+      main: "#F13C20",
+    },
+    info: {
+      main: "#C5CBE3",
+      contrastText: "#EFE2BA",
+    },
+    background: {
+      default: "#E9EEEE",
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+});
+
+export default theme;
