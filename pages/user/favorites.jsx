bce9871b6a99c35af5bb665e4440ec5a7cbc87e7
Satoe Sakonjo <sakonjo310@gmail.com>
2023-01-07 15:10:44 -0800

    Show all the favorite recipes

--- pages/user/favorites.jsx
+++ pages/user/favorites.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import Link from "next/link";
+import { getFavorites } from "../../lib/favourite";
 
-function Favorites() {
+const Favourites = (props) => {
+
+  const results = props.recipes.map((item) => (
+    <ImageListItem key={item.idDrink}>
+      <IconButton
+        onClick={(event) => console.log("apple")}
+        sx={{
+          position: "absolute",
+          top: "5px",
+          right: "5px",
+        }}
+      >
+        <FavoriteBorderIcon
+          sx={{
+            fontSize: 35,
+            color: "disabled",
+            fontWeight: 0.5,
+          }}
+        />
+      </IconButton>
+      <img
+        src={`${item.strDrinkThumb}?w=150&fit=crop`}
+        alt={item.strDrink}
+        loading="lazy"
+      />
+      <Link href={`/cocktail/${item.idDrink}`}>
+        <ImageListItemBar title={item.strDrink} subtitle={item.strCategory} />
+      </Link>
+    </ImageListItem>
+  ));
   return (
-    <div>Favorites</div>
-  )
+    <>
+      <ImageList sx={{ width: 1000, height: 1000 }} cols={3}>
+        {results}
+      </ImageList>
+    </>
+  );
+};
+
+export async function getServerSideProps(context) {
+  const sessionToken = context.req.cookies['next-auth.session-token']
+  const recipes = await getFavorites(sessionToken);
+  return {
+    props: {
+      recipes
+    },
+  };
 }
 
-export default Favorites
+export default Favourites;
