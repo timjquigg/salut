1637abfdf4a5c703277728fc379b47b25bc75d59
Satoe Sakonjo <sakonjo310@gmail.com>
2023-01-09 19:55:53 -0800

    Improve styles on search page

--- pages/search/index.jsx
+++ pages/search/index.jsx
@@ -6,10 +6,53 @@ import useSearch from "../../custom_hook/useSearch";
 import { useState } from "react";
 import Button from "@mui/material/Button";
 import Image from "next/image";
-import Box from "@mui/material/Box";
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import theme from "../../src/theme";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 const Search = (props) => {
-  const [filterForm, setFilterForm] = useState(true);
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   const {
     enteredSearch,
     changeHandler,
@@ -22,48 +65,41 @@ const Search = (props) => {
   } = useSearch();
 
   return (
-    <SearchContainer marginTop={39}>
+    <SearchContainer marginTop={15}>
       <Box
         sx={{
-          p: 2.5,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
         }}
       >
-        <Image
-          src={"/salut_logo.png"}
-          alt="logo"
-          width="330"
-          height="90"
-          quality={100}
-        />
+        <Typography sx={{fontFamily: theme.typography.fontFamily[0], fontSize: '40px'}}>Look for recipes</Typography>
+        <Box sx={{ width: '100%' }}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+              <Tab label="Search by ingredients" {...a11yProps(0)} />
+              <Tab label="Search by keywords" {...a11yProps(1)} />
+            </Tabs>
+          </Box>
+          <TabPanel value={value} index={0}>
+            <FilterForm
+              options={props.ingredients}
+              filterKeywords={filterKeywords}
+              inputFilterKeywords={inputFilterKeywords}
+              onChange={changeFilterHandler}
+              onInputChange={changeInputFilterHandler}
+              onClick={submitFilterHandler}
+            />
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <KeywordForm
+              enteredSearch={enteredSearch}
+              changeHandler={changeHandler}
+              submitHandler={submitHandler}
+            />
+          </TabPanel>
+        </Box>
       </Box>
-
-      {filterForm ? (
-        <FilterForm
-          options={props.ingredients}
-          filterKeywords={filterKeywords}
-          inputFilterKeywords={inputFilterKeywords}
-          onChange={changeFilterHandler}
-          onInputChange={changeInputFilterHandler}
-          onClick={submitFilterHandler}
-        />
-      ) : (
-        <KeywordForm
-          enteredSearch={enteredSearch}
-          changeHandler={changeHandler}
-          submitHandler={submitHandler}
-        />
-      )}
-
-      <Button
-        variant="contained"
-        onClick={() => setFilterForm((prev) => !prev)}
-        sx={{
-          width: "12%",
-          mt: 2,
-        }}
-      >
-        Search by {filterForm ? "Keyword" : "Filter"}
-      </Button>
     </SearchContainer>
   );
 };
