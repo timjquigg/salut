703a0c65a6b219b3daa14838458a8f4baaefceaf
Francisco <kikopocampo@gmail.com>
2023-01-14 03:12:26 -0700

    created cocktail client

--- pages/api/cocktail.js
+++ pages/api/cocktail.js
@@ -1,9 +1,15 @@
-import { getCocktailsBasedOnInventory } from "../../lib/cocktail";
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+import { getUserCreatedCocktails } from "../../lib/cocktail";
 
 async function Handler(req, res) {
   if (req.method === "GET") {
-    const inventory = req.query;
-    const cocktails = await getCocktailsBasedOnInventory(inventory);
-    return cocktails;
+    // console.log("USERID", req.query.userId);
+    const { userId } = req.query;
+    const cocktails = await getUserCreatedCocktails(userId);
+
+    res.status(200).json({ cocktails });
   }
 }
+
+export default Handler;
