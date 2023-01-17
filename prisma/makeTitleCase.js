5cd3c4a5c0acd83c0e723387a36bbb9f3f7145dc
Tim Quigg <timjquigg@gmail.com>
2023-01-17 14:18:11 -0700

    Updated the seed function to change drink titles to Title Case (accounting for "and");

--- prisma/makeTitleCase.js
+++ prisma/makeTitleCase.js
@@ -1,8 +1,8 @@
 const makeTitleCase = (input) => {
   input = input.trim().split(" ");
   const titleCase = input.map((el) => {
-    if (el === "and") {
-      return el;
+    if (el.toLowerCase() === "and") {
+      return el.toLowerCase();
     }
     return el[0].toUpperCase() + el.slice(1).toLowerCase();
   });
